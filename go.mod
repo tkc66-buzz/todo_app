e4ffb5b24d3d9e3e8713f2feb6f8fcc18384abfb
tkc66 <worktkc2018@gmail.com>
2023-01-07 06:54:01 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tkc66-buzz
+
+go 1.19
+
+require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tkc66-buzz/todo_app/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
