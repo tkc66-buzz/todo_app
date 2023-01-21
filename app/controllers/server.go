a42aa3679b46d928d00600e0673332283b6c7943
tkc66 <worktkc2018@gmail.com>
2023-01-21 23:16:19 +0900

    add: layout html

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,10 +1,22 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/tkc66-buzz/todo_app/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, fileNames ...string) {
+	var files []string
+	for _, fn := range fileNames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", fn))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
