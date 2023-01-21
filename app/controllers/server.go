af1101919b8ddf5e85b0e56ef4b4954ae6dc14bd
tkc66 <worktkc2018@gmail.com>
2023-01-21 23:05:53 +0900

    add: bootstrap and jquery

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -7,6 +7,9 @@ import (
 )
 
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
